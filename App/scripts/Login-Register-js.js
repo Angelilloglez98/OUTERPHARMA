38317aa22900c5c6d82efe1ef79679c0fc052468
Angelilloglez98 <127234599+Angelilloglez98@users.noreply.github.com>
2023-03-10 22:09:20 +0100

    correcion de bugs

--- App/scripts/Login-Register-js.js
+++ App/scripts/Login-Register-js.js
@@ -1,9 +1,44 @@
 let formularios=document.querySelector('#formularios');
 let btnregistrar=document.querySelector('#moverRegister');
 let btnLogin=document.querySelector('#moverLogin');
+const form = document.querySelector('.Login');
+const emailInput = form.querySelector('input[type="email"]');
+const passwordInput = form.querySelector('input[type="password"]');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+  
+  // Validar el campo de correo electrónico
+  if (!email) {
+    alert('Por favor, introduce tu correo electrónico');
+    emailInput.focus();
+    return;
+  }
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    alert('Por favor, introduce un correo electrónico válido');
+    emailInput.focus();
+    return;
+  }
+  
+  // Validar el campo de contraseña
+  if (!password) {
+    alert('Por favor, introduce tu contraseña');
+    passwordInput.focus();
+    return;
+  }
+  if (password.length < 6) {
+    alert('La contraseña debe tener al menos 6 caracteres');
+    passwordInput.focus();
+    return;
+  }
+  
+  // Si los campos son válidos, enviar el formulario
+  form.submit();
+});
 
 btnregistrar.onclick=()=>{
-    console.log('caca');
     activarRegistrar();
 }
 btnLogin.onclick=()=>{
