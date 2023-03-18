983938a671ba41b0bbb6b767fca669e6128f83a3
Angelilloglez98 <127234599+Angelilloglez98@users.noreply.github.com>
2023-03-18 01:20:09 +0100

    muchas modificaciones

--- App/BaseDatos/devProductosMasVendidos.php
+++ App/BaseDatos/devProductosMasVendidos.php
@@ -1,15 +1,20 @@
 <?php
     require('./conexionDB.php');
     session_start();
-
+    $fecha;
     $registros=array();
-
+    if (isset($_GET['fecha'])) {
+        $fecha=$_GET['fecha'];
+    }else {
+        $fecha='2023';
+    }
+   
     $correo=$_SESSION['CorreoFarmacia'];
 
     $sql="SELECT p.Nombre, SUM(v.Cantidad) as CantidadVendida 
     FROM VENTAS v JOIN PRODUCTOS p ON v.IdProducto = p.IdProducto JOIN EMPLEADOS e ON v.nEmpleado = e.nEmpleado JOIN FARMACIAS f ON e.CcorreoFarmacia = f.Ccorreo
-    WHERE f.Ccorreo = '$correo' 
-    AND YEAR(v.Fecha) = YEAR(CURDATE())
+    WHERE f.Ccorreo = '$correo'
+    AND YEAR(v.Fecha) = '$fecha'
     GROUP BY p.Nombre
     ORDER BY CantidadVendida DESC;";
 
--- App/scripts/graficoLineal.js
+++ App/scripts/graficoLineal.js
@@ -100,78 +100,6 @@ function datosVentasPedidos() {
     
 }
 
-
-
-
-function datosGraficoCircular() {
-    let nombreMedicamento = [];
-    let cantidadPorMedicamento = [];
-    fetch('http://localhost/OuterPharma/App/BaseDatos/devProductosMasVendidos.php')
-        .then(data => data.json())
-        .then(res => {
-            res.forEach(element => {
-
-                nombreMedicamento.push(element.Nombre);
-                cantidadPorMedicamento.push(element.CantidadVendida);
-
-            });
-        GraficoCircular(nombreMedicamento,cantidadPorMedicamento);
-        })
-    
-}
-
-function GraficoCircular(nombresMedicamentos,cantidadMedicamento) {
-    var graficoCircular = document.getElementById('graficoCircular').getContext('2d');
-
-var data = {
-    labels: nombresMedicamentos,
-    datasets: [{
-        label: 'Cantidad',
-        data: cantidadMedicamento,
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.7)',
-            'rgba(54, 162, 235, 0.7)',
-            'rgba(255, 240, 86, 0.7)',
-            'rgba(140,200,43,0.7)',
-            'rgba(255,140,21,0.7)'
-        ],
-        borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 240, 86, 1)',
-            'rgba(140,200,43,1)',
-            'rgba(255,140,21,1)'
-        ],
-        borderWidth: 1
-    }]
-};
-
-var options = {
-    plugins:{
-        legend:{
-            display:true,
-            position:'right'
-        }
-    },
-    responsive: true,
-    aspectRatio: 2,
-    scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true
-            }
-        }]
-    }
-};
-
-new Chart(graficoCircular, {
-    type: 'pie',
-    data: data,
-    options: options
-});
-
-}
-
 function graficoLineal(linea1,linea2) {
     const ctx = document.getElementById('myChart');
 
@@ -200,10 +128,7 @@ function graficoLineal(linea1,linea2) {
     });
 }
 
-function pintarGraficos() {
-     datosGraficoCircular();
-    datosVentasPedidos();
-    
-}
+datosVentasPedidos(); 
 
-pintarGraficos();
+
+
