20ca5168e5b5fe37112955f14217205a64ee7998
Rubiniun <122886578+Rubiniun@users.noreply.github.com>
2023-03-20 19:18:54 +0100

    cambios tabla y fetchs

--- App/scripts/pedidos.js
+++ App/scripts/pedidos.js
@@ -8,4 +8,33 @@ window.onload = () =>{
 
     }));
 
+    const buscarMed = async(datos) => {
+
+        const options = {
+            method: 'GET',
+            redirect: 'follow',
+            Headers: {'Accept':'aplication/jason'}
+        }
+
+        const url = "https://cima.aemps.es/cima/rest/medicamentos?";
+        const busqueda = "nombre="
+        // if(isNaN(datos)){url+="s?"}
+        // else{url+="?"}
+
+        return fetch(url+busqueda+datos, options)
+        .then(response => response.json())
+        .then(medicamentos => {return medicamentos})
+        .catch(e => {console.error("ERROR: ", e.message)});
+
+    }
+
+    const busqueda = document.querySelector('input[type="search"]');
+    console.log(busqueda.value); 
+
+    buscarMed("paracetamol").then((response) => {
+        console.log(response);
+    })
+
+
+
 } 
