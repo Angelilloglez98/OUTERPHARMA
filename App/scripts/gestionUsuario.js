3559ff5e9e240ff4836f6fa21db68e77d7ffd70f
juanjods99 <ajudsanchez@gmail.com>
2023-03-21 19:08:05 +0100

    Progreso

--- App/scripts/gestionUsuario.js
+++ App/scripts/gestionUsuario.js
@@ -1,68 +1,71 @@
-window.onload=()=>{
+window.onload = () => {
 
     fetch('http://localhost/OuterPharma/App/BaseDatos/devEmpleados.php')
     .then(response => response.json())
     .then(registro => registro.forEach(element => {
-        Pintar(document.querySelector('.contenedorUser'),element.nombre)
-        let add = document.querySelector("#add");
-        let del = document.querySelector("#delete");
-        let edit = document.querySelector("#edit");
-
-        
-        function Pintar(elemento, mensaje) {
-            let div=document.createElement('div');
-            div.classList.add('user');
-            let p=document.createElement('p');
-            let texto=document.createTextNode(mensaje);
-            p.appendChild(texto);
-            div.appendChild(p);
-            elemento.appendChild(div);
-
-            p.addEventListener('click',()=>{
-                console.log("hola");
-            })
-            
-        }
-
-        function pintarRol(elemento) {
-
-            
-
-            let inputRol=document.createElement('input');
-            inputRol.type='radio';
-            inputRol.value='usuario'
-            inputRol.name="rol"
-            inputRol.id = 'usuarios';
-            elemento.appendChild(inputRol);
-            const label1 = document.createElement('label');
-            label1.htmlFor = 'usuario';
-            label1.appendChild(document.createTextNode('usuario'));
-            element.body.appendChild(label1);
-            let inputRol2=document.createElement('input');
-            inputRol2.type='radio';
-            inputRol2.value='administrador'
-            inputRol2.name="rol"
-            inputRol2.id = 'administradors';
-            elemento.appendChild(inputRol2); 
-            const label2 = document.createElement('label');
-            label2.htmlFor = 'administrador';
-            label2.appendChild(document.createTextNode('admin'));
-            element.body.appendChild(label2);
-        }
-    
+        Pintar(document.querySelector('.contenedorUser'), element.nombre)
     }));
 
-    add.onclick=()=>{console.log("hola");}
-        add.addEventListener('click',()=>{
-            console.log("hola");
-        })
-        del.addEventListener('click',()=>{
-            pintarRol(div);
-        })
-        edit.addEventListener('click',()=>{
-            pintarRol(div);
-        })
+    function Pintar(elemento, mensaje) {
+        let div = document.createElement('div');
+        let p = document.createElement('p');
+        let texto = document.createTextNode(mensaje);
+        let beli = document.createElement('button');
+        let bedi = document.createElement('button');
+        let trash = document.createElement('i');
+        let edit = document.createElement('i');
 
+        beli.appendChild(trash);
+        bedi.appendChild(edit);
+        p.appendChild(texto);
+        p.appendChild(beli);
+        p.appendChild(bedi);
+        div.appendChild(p);
 
- 
+        elemento.appendChild(div);
+        trash.classList.add("bx", "bxs-trash");
+        edit.classList.add("bx", "bxs-edit");
+        beli.setAttribute("id", "delete");
+        bedi.setAttribute("id", "edit");
+
+        trash.addEventListener("click", function() {
+            // Redirigir a la página de eliminar
+            window.location.href = "gestionEliminar.html";
+        });
+        
+        edit.addEventListener("click", function(element) {
+            ventanaEmergente();
+
+        });
+    }
+
+    function ventanaEmergente() {
+        let div = document.createElement('div');
+        div.setAttribute("id","emergente");
+        div.classList.add("emergente");
+        let pName = document.createElement('p');
+        let txtName = document.createTextNode("Nombre:");
+        pName.appendChild(txtName);
+        div.appendChild(pName);
+        let inputName = document.createElement("input");
+        inputName.type = "text";
+        inputName.value = "hola"
+        div.appendChild(inputName);
+        let pFarmacia = document.createElement('p');
+        let txtFarmacia = document.createTextNode("Farmacia:");
+        pFarmacia.appendChild(txtFarmacia);
+        div.appendChild(pFarmacia);
+        let inputFarmacia = document.createElement("input");
+        inputFarmacia.type = "text";
+        inputFarmacia.value = "Farmacia";
+        div.appendChild(inputFarmacia);
+        let pRol = document.createElement('p');
+        let txtRol = document.createTextNode("Cargo:");
+        pRol.appendChild(txtRol);
+        div.appendChild(pRol);
+        
+
+        
+        document.body.appendChild(div);
+    }
 }
