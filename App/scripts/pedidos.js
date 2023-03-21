9c9b4098b30ab9f745fcce342c93dba7ca7463f4
Rubiniun <122886578+Rubiniun@users.noreply.github.com>
2023-03-21 19:50:05 +0100

    TABLA DE PEDIDOS CREADA

--- App/scripts/pedidos.js
+++ App/scripts/pedidos.js
@@ -1,5 +1,7 @@
 window.onload = () =>{
 
+    //IMPLEMENTAR LIMPIAR LA TABLA
+
     fetch('http://localhost/OuterPharma/App/BaseDatos/devProveedores.php')
     .then(response => response.json())
     .then(registro => registro.forEach(element => {
@@ -13,7 +15,7 @@ window.onload = () =>{
         const options = {
             method: 'GET',
             redirect: 'follow',
-            Headers: {'Accept':'aplication/jason'}
+            Headers: {'Accept':'aplication/json'}
         }
 
         const url = "https://cima.aemps.es/cima/rest/medicamentos?";
@@ -30,20 +32,70 @@ window.onload = () =>{
 
     const busqueda = document.querySelector('input[type="search"]');
 
-    busqueda.addEventListener("keydown", (event) => {
+    const creatRow = (data) =>{
 
-        if (event.isComposing || event.keyCode === 'Enter') {
-          console.log("Si");
+        var tableBody = document.querySelector("#buscarMed");
+        var tr = document.createElement('tr');
+        var th = document.createElement('th');
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        th.appendChild(checkbox);
+        tr.appendChild(th);
+
+        for (const i in data) {
+            let td = document.createElement('td');
+            if (i == 'nombre' || i == 'vtm' || i == 'formaFarmaceutica' || i == 'viasAdministracion' || i == 'labtitular' || i == 'cpresc') {
+                
+                if(i == 'vtm' || i == 'formaFarmaceutica' || i == 'viasAdministracion'){
+
+                    for (const j in data[i]) {
+                        if(j == 'nombre'){
+                            let info = document.createTextNode(data[i][j]);
+                            td.appendChild(info); 
+                        }
+                    }
+
+                }else{
+                  let info = document.createTextNode(data[i]);
+                  td.appendChild(info);
+                }
+
+                
+                tr.appendChild(td);
+                
+            }
+        }
+
+        tableBody.appendChild(tr);
+
+    }
+
+    busqueda.addEventListener("keydown", (event) => {
+ 
+        if (event.key === 'Enter' && busqueda.value != '') {
+            var datos = busqueda.value;
+            busqueda.value = "";
+          
+            buscarMed(datos).then((response) => {
+                console.log(response.resultados);
+                console.log(response.resultados[0].nombre);
+                console.log(response.resultados[0].vtm.nombre);
+                console.log(response.resultados[0].formaFarmaceutica.nombre);
+                console.log(response.resultados[0].viasAdministracion[0].nombre);
+                console.log(response.resultados[0].labtitular);
+                console.log(response.resultados[0].cpresc);
+
+                response.resultados.forEach(element => {
+                    
+                    creatRow(element);
+                    
+                })
+
+            })
+
         }
         
       });
-
-    console.log(busqueda.value); 
-
-    buscarMed("paracetamol").then((response) => {
-        console.log(response);
-    })
-
-
 
 } 
