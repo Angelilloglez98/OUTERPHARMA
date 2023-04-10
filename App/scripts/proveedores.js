218293f836f2e04c34206462e738ed392df207c8
Rubiniun <122886578+Rubiniun@users.noreply.github.com>
2023-04-10 20:21:44 +0200

    arreglo cosas y aniadir proveedores v0.0.1

--- App/scripts/proveedores.js
+++ App/scripts/proveedores.js
@@ -2,9 +2,76 @@ window.onload = () =>{
 
     const newProv = document.querySelector(".newProv");
     const button = document.createElement('button')
+    const hideOnBush = document.createElement('div');
+    const form = document.createElement("form");
+    const nombreInput = document.createElement("input");
+    const direccionInput = document.createElement("input");
+    const telefonoInput = document.createElement("input");
+    const paginaWebInput = document.createElement("input");
+
+    nombreInput.setAttribute("type", "text");
+    nombreInput.setAttribute("name", "nombre");
+    nombreInput.setAttribute("placeholder", "Nombre"); 
+
+    direccionInput.setAttribute("type", "text");
+    direccionInput.setAttribute("name", "direccion");
+    direccionInput.setAttribute("placeholder", "Dirección");
+    
+    telefonoInput.setAttribute("type", "tel");
+    telefonoInput.setAttribute("name", "telefono");
+    telefonoInput.setAttribute("placeholder", "Teléfono");  
+
+    paginaWebInput.setAttribute("type", "url");
+    paginaWebInput.setAttribute("name", "pagina-web");
+    paginaWebInput.setAttribute("placeholder", "Página Web");
+   
     button.className = 'addProv';
     button. textContent = 'Añadir +';
+    hideOnBush.classList.add('card');
+    hideOnBush.style.display = 'none';
+    form.classList.add('card-body');
+
+    form.appendChild(nombreInput);
+    form.appendChild(direccionInput);
+    form.appendChild(telefonoInput);
+    form.appendChild(paginaWebInput);
+
+    hideOnBush.appendChild(form);
+
     newProv.appendChild(button);
+    newProv.appendChild(hideOnBush);
+
+    button.onclick = ()=>{
+        hideOnBush.style.display = 'flex';
+        const form = {
+            title: "Formulario",
+            html: `
+              <input type="text" name="nombre" placeholder="Nombre" class="swal2-input">
+              <input type="text" name="direccion" placeholder="Dirección" class="swal2-input">
+              <input type="tel" name="telefono" placeholder="Teléfono" class="swal2-input">
+              <input type="url" name="pagina-web" placeholder="Página Web" class="swal2-input">
+            `,
+            focusConfirm: false,
+            preConfirm: () => {
+              // Retorna un objeto con los valores de los campos del formulario
+              return {
+                nombre: document.getElementsByName("nombre")[0].value,
+                direccion: document.getElementsByName("direccion")[0].value,
+                telefono: document.getElementsByName("telefono")[0].value,
+                paginaWeb: document.getElementsByName("pagina-web")[0].value,
+              };
+            },
+          };
+      
+          // Muestra la ventana modal con el formulario
+          Swal.fire(form).then((result) => {
+            // Si el usuario ha enviado el formulario, muestra los valores de los campos
+            if (result.isConfirmed) {
+              const formData = result.value;
+              console.log(formData);
+            }
+          });
+    }
 
     const buscarProv = async()=>{
 
