0d332d865d3449b8b1441a0fe715ee3a83786386
Rubiniun <122886578+Rubiniun@users.noreply.github.com>
2023-04-11 19:31:17 +0200

    insert proveedores v0.0.2

--- App/scripts/proveedores.js
+++ App/scripts/proveedores.js
@@ -1,143 +1,127 @@
 window.onload = () =>{
 
-    const newProv = document.querySelector(".newProv");
-    const button = document.createElement('button')
-    const hideOnBush = document.createElement('div');
-    const form = document.createElement("form");
-    const nombreInput = document.createElement("input");
-    const direccionInput = document.createElement("input");
-    const telefonoInput = document.createElement("input");
-    const paginaWebInput = document.createElement("input");
+  const newProv = document.querySelector(".newProv");
+  const button = document.createElement('button')
+  const hideOnBush = document.createElement('div');
+  const form = document.createElement("form");
+  
+  button.className = 'addProv';
+  button. textContent = 'Añadir +';
+  newProv.appendChild(button);
 
-    nombreInput.setAttribute("type", "text");
-    nombreInput.setAttribute("name", "nombre");
-    nombreInput.setAttribute("placeholder", "Nombre"); 
+  const buscarProv = async()=>{
 
-    direccionInput.setAttribute("type", "text");
-    direccionInput.setAttribute("name", "direccion");
-    direccionInput.setAttribute("placeholder", "Dirección");
-    
-    telefonoInput.setAttribute("type", "tel");
-    telefonoInput.setAttribute("name", "telefono");
-    telefonoInput.setAttribute("placeholder", "Teléfono");  
+      const options = {
+          method: 'GET',
+          redirect: 'follow',
+          Headers: {'Accept':'aplication/json'}
+      }
 
-    paginaWebInput.setAttribute("type", "url");
-    paginaWebInput.setAttribute("name", "pagina-web");
-    paginaWebInput.setAttribute("placeholder", "Página Web");
-   
-    button.className = 'addProv';
-    button. textContent = 'Añadir +';
-    hideOnBush.classList.add('card');
-    hideOnBush.style.display = 'none';
-    form.classList.add('card-body');
-
-    form.appendChild(nombreInput);
-    form.appendChild(direccionInput);
-    form.appendChild(telefonoInput);
-    form.appendChild(paginaWebInput);
-
-    hideOnBush.appendChild(form);
-
-    newProv.appendChild(button);
-    newProv.appendChild(hideOnBush);
-
-    button.onclick = ()=>{
-        hideOnBush.style.display = 'flex';
-        const form = {
-            title: "Formulario",
-            html: `
-              <input type="text" name="nombre" placeholder="Nombre" class="swal2-input">
-              <input type="text" name="direccion" placeholder="Dirección" class="swal2-input">
-              <input type="tel" name="telefono" placeholder="Teléfono" class="swal2-input">
-              <input type="url" name="pagina-web" placeholder="Página Web" class="swal2-input">
-            `,
-            focusConfirm: false,
-            preConfirm: () => {
-              // Retorna un objeto con los valores de los campos del formulario
-              return {
-                nombre: document.getElementsByName("nombre")[0].value,
-                direccion: document.getElementsByName("direccion")[0].value,
-                telefono: document.getElementsByName("telefono")[0].value,
-                paginaWeb: document.getElementsByName("pagina-web")[0].value,
-              };
-            },
-          };
+      var url = 'http://localhost/OuterPharma/App/BaseDatos/devProveedores.php';
       
-          // Muestra la ventana modal con el formulario
-          Swal.fire(form).then((result) => {
-            // Si el usuario ha enviado el formulario, muestra los valores de los campos
-            if (result.isConfirmed) {
-              const formData = result.value;
-              console.log(formData);
-            }
-          });
+      return fetch(url, options)
+      .then(response => response.json())
+      .then(medicamentos => {return medicamentos})
+      .catch(e => {console.error("ERROR: ", e.message)});
+
+  }
+
+  const createProv = (data) => {
+
+      data.forEach(element => {
+
+          let showProv = document.querySelector(".showProv");
+          let cardProv = document.createElement("div");
+          cardProv.className = "tarjeta-proveedor";
+
+          for (const atribute in element) {
+
+              let info = document.createTextNode(element[atribute]);
+              let prov = '';
+              if(atribute === 'nombre'){
+
+                  prov = document.createElement("h5");
+                  prov.appendChild(info);
+
+              }else if(atribute === 'Link'){
+                  prov = document.createElement("a");
+                  prov.href = `${element[atribute]}`;
+                  prov.textContent = 'Pagina Web';
+                  prov.target = '_blank';
+              }else{
+
+                  prov = document.createElement("p");
+                  prov.appendChild(info);
+
+              }
+
+              cardProv.appendChild(prov);
+
+              console.log(element);
+
+          }
+
+          showProv.appendChild(cardProv);
+
+      });
+
+  }
+
+  buscarProv().then((response)=>{
+
+    createProv(response);
+
+  });
+
+  const insertProv = async(datos)=>{
+
+    const options = {
+        method: 'GET',
+        redirect: 'follow',
+        body: datos,
+        Headers: {'Accept':'aplication/json'}
     }
 
-    const buscarProv = async()=>{
+    var url = 'http://localhost/OuterPharma/App/BaseDatos/insertProveedores.php';
+    
+    return fetch(url, options)
+    .then(response => response.json())
+    .then(result => {return result})
+    .catch(e => {console.error("ERROR: ", e.message)});
 
-        const options = {
-            method: 'GET',
-            redirect: 'follow',
-            Headers: {'Accept':'aplication/json'}
+}
+
+  button.onclick = ()=>{
+    hideOnBush.style.display = 'flex';
+    const form = {
+        title: "Formulario",
+        html: `
+          <input type="text" name="nombre" placeholder="Nombre" class="swal2-input">
+          <input type="text" name="direccion" placeholder="Dirección" class="swal2-input">
+          <input type="tel" name="telefono" placeholder="Teléfono" class="swal2-input">
+          <input type="url" name="pagina-web" placeholder="Página Web" class="swal2-input">
+        `,
+        focusConfirm: false,
+        preConfirm: () => {
+          // Retorna un objeto con los valores de los campos del formulario
+          return {
+            nombre: document.getElementsByName("nombre")[0].value,
+            direccion: document.getElementsByName("direccion")[0].value,
+            telefono: document.getElementsByName("telefono")[0].value,
+            paginaWeb: document.getElementsByName("pagina-web")[0].value,
+          };
+        },
+      };
+  
+      // Muestra la ventana modal con el formulario
+      Swal.fire(form).then((result) => {
+        // Si el usuario ha enviado el formulario, muestra los valores de los campos
+        if (result.isConfirmed) {
+          const formData = result.value;
+          insertProv(formData).then(response => console.log(formData, response));
+    
         }
-
-        var url = 'http://localhost/OuterPharma/App/BaseDatos/devProveedores.php';
-        
-        return fetch(url, options)
-        .then(response => response.json())
-        .then(medicamentos => {return medicamentos})
-        .catch(e => {console.error("ERROR: ", e.message)});
-
-    }
-
-    const createProv = (data) => {
-
-        data.forEach(element => {
-
-            let showProv = document.querySelector(".showProv");
-            let cardProv = document.createElement("div");
-            cardProv.className = "tarjeta-proveedor";
-
-            for (const atribute in element) {
-
-                let info = document.createTextNode(element[atribute]);
-                let prov = '';
-                if(atribute === 'nombre'){
-
-                    prov = document.createElement("h5");
-                    prov.appendChild(info);
-
-                }else if(atribute === 'Link'){
-                    prov = document.createElement("a");
-                    prov.href = `${element[atribute]}`;
-                    prov.textContent = 'Pagina Web';
-                    prov.target = '_blank';
-                }else{
-
-                    prov = document.createElement("p");
-                    prov.appendChild(info);
-
-                }
-
-                cardProv.appendChild(prov);
-
-                console.log(element);
-
-            }
-
-            showProv.appendChild(cardProv);
-
-
-        });
-
-    }
-
-    buscarProv().then((response)=>{
-
-        createProv(response);
-
-    });
-
-
+      });
+}
 
 }
